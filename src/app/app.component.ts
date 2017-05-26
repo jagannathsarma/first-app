326b0f9efeaf2b8dae5415ce6b7b1f5787e0d52c
Jags <jags@Jags-MacBook-Pro-2.local>
2017-05-26 23:59:29 +0530

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app works!';
+  pageTitle: string = 'first-app works!';
 }
